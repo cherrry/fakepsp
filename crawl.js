f75c2726ad28455bbf4dc50fe8ba54ec491d0c6e
Cherry Ng <cherry92@gmail.com>
2017-12-31 11:40:19 +0800

    Simple crawler for extracting PTH <> Cantonese mapping.

---
+++ crawl.js
@@ -0,0 +1,55 @@
+const Crawler = require('crawler');
+
+const RE_ALPHA = /^([a-zü]+)/;
+const RE_WORD = /^([^a-z]+)\s*([a-z]+[1-6])/;
+const c = new Crawler();
+
+const run = (type, link) => new Promise((resolve, reject) => {
+  c.queue({
+    uri: `http://corpus.eduhk.hk/cantonese/scramble/${type}/${link}.html`,
+    callback: (err, {$}, done) => {
+      if (err != null) {
+        reject(err);
+        return done();
+      }
+      const rows = $('table tr');
+      const pth = $(rows[1]).find('td:first-child').text().trim().match(RE_ALPHA)[1];
+
+      let results = [];
+      for (let i = 1; i < rows.length; ++i) {
+        const offset = i === 1 ? 1 : 0;
+        const cols = $(rows[i]).find('td');
+        const jyut = $(cols[offset]).text().trim();
+        $(cols[offset+1]).text().split(',').forEach((w) => {
+          if (!RE_WORD.test(w.trim())) {
+            return;
+          }
+          const match = RE_WORD.exec(w.trim());
+          results.push({
+            group: {pth, jyut},
+            word: {word: match[1].trim(), pth: '', jyut: match[2].trim()},
+          });
+        });
+      }
+      resolve(results);
+      return done();
+    },
+  });
+});
+
+const finals = [
+  'a', 'ia', 'ua', 'o', 'uo', 'e', 'ie',
+  'ue', 'i', 'i_1', 'i_2', 'u1', 'u2', 'ai',
+  'uai', 'ei', 'ui', 'ao', 'iao', 'ou', 'iu',
+  'an', 'ian', 'uan1', 'uan2', 'en', 'in', 'un1',
+  'un2', 'an', 'iang', 'uang', 'eng', 'ing', 'ueng',
+  'ong', 'iong',
+];
+
+Promise.all(finals.map((f) => run('finals', f))).then((results) => {
+  let finalResult = [];
+  results.forEach((result) => {
+    result.forEach((value) => finalResult.push(value));
+  });
+  console.log(JSON.stringify(finalResult, null, 2));
+});
